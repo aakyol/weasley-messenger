3b2fb8a09920eb4fd4364e27dbb700bfc6f80a4b
Aydın Akyol <aydinaakyol@gmail.com>
2019-02-03 13:04:21 +0000

    location: pull back fast location updates

--- app/src/main/java/app/aakyol/weasleymessenger/service/LocationService.java
+++ app/src/main/java/app/aakyol/weasleymessenger/service/LocationService.java
@@ -44,8 +44,8 @@ public class LocationService extends Service {
 
     private LocationRequest locationRequest;
 
-    private long UPDATE_INTERVAL = /*15 **/ 60 * 1000;
-    private long FASTEST_INTERVAL = /*10 **/ 60 * 1000;
+    private long UPDATE_INTERVAL = 15 * 60 * 1000;
+    private long FASTEST_INTERVAL = 10 * 60 * 1000;
 
     private Handler locationHandler = new Handler();
     private Context locationServiceContext;
